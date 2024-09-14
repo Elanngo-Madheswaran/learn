a544b459f9f5abb39994574795db1bba3e64338f
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-09-14 19:32:29 +0530

    added welcome msg to the front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,49 +13,53 @@
 </script>
 
 <!-- Bind the dark mode class dynamically -->
-<div class:dark={isDarkMode} class="min-h-screen flex">
+<div class:dark={isDarkMode} class="flex">
     <!-- Sidebar and Links -->
-    <Sidebar >
-        <SidebarWrapper class="h-screen">
-            <SidebarGroup>
-                <SidebarBrand {site} />
-                <SidebarItem label="Student Page" href="./student">
-                    <svelte:fragment slot="icon">
-                        <ChartPieSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                    </svelte:fragment>
-                </SidebarItem>
-                <SidebarItem label="Teacher Page" href="./teacher" {spanClass}>
-                    <svelte:fragment slot="icon">
-                        <GridSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                    </svelte:fragment>
-                </SidebarItem>
-                <SidebarItem label="Admin Page" {spanClass} href="./admin">
-                    <svelte:fragment slot="icon">
-                        <MailBoxSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                    </svelte:fragment>
-                </SidebarItem>
-                <SidebarItem label="Users">
-                    <svelte:fragment slot="icon">
-                        <UserSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                    </svelte:fragment>
-                </SidebarItem>
-                <SidebarItem label="Sign In">
-                    <svelte:fragment slot="icon">
-                        <ArrowRightToBracketOutline class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                    </svelte:fragment>
-                </SidebarItem>
-                <SidebarItem label="Sign Up">
-                    <svelte:fragment slot="icon">
-                        <EditOutline class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
-                    </svelte:fragment>
-                </SidebarItem>
-            </SidebarGroup>
-        </SidebarWrapper>
-    </Sidebar>
+    
+    <div>
+        <Sidebar >
+            <SidebarWrapper class="h-screen">
+                <SidebarGroup>
+                    <SidebarBrand {site} />
+                    <SidebarItem label="Student Page" href="./student">
+                        <svelte:fragment slot="icon">
+                            <ChartPieSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        </svelte:fragment>
+                    </SidebarItem>
+                    <SidebarItem label="Teacher Page" href="./teacher" {spanClass}>
+                        <svelte:fragment slot="icon">
+                            <GridSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        </svelte:fragment>
+                    </SidebarItem>
+                    <SidebarItem label="Admin Page" {spanClass} href="./admin">
+                        <svelte:fragment slot="icon">
+                            <MailBoxSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        </svelte:fragment>
+                    </SidebarItem>
+                    <SidebarItem label="Users">
+                        <svelte:fragment slot="icon">
+                            <UserSolid class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        </svelte:fragment>
+                    </SidebarItem>
+                    <SidebarItem label="Sign In">
+                        <svelte:fragment slot="icon">
+                            <ArrowRightToBracketOutline class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        </svelte:fragment>
+                    </SidebarItem>
+                    <SidebarItem label="Sign Up">
+                        <svelte:fragment slot="icon">
+                            <EditOutline class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                        </svelte:fragment>
+                    </SidebarItem>
+                </SidebarGroup>
+            </SidebarWrapper>
+        </Sidebar>
+    </div>
+
 
     <!-- Content area (can be filled with other content) -->
-    <div class="flex-1 p-5 text-white">
-        <!-- Your content goes here -->
+    <div class="flex-1 p-5 text-white sm:block hidden">
+        <h1 class="text-6xl text-center">Welcome to Our app</h1>
     </div>
 </div>
 
